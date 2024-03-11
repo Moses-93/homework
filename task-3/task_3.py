a15742d06d1015f65e00824084d3200e6da565dc
Moses-93 <moieseenko2001@gmail.com>
2024-03-11 19:40:03 +0200

    task_3_add_func

--- task-3/task_3.py
+++ task-3/task_3.py
@@ -11,11 +11,13 @@ raw_numbers = [
     "38050-111-22-22",
     "38050 111 22 11   ",
 ]
-try:
-    regex = "[^0-9]" # регулярний вираз для символів, які ми хочемо видалити
-    formated_phone_num = ["+38" + re.sub(regex, "", number.replace("38", "")) for number in raw_numbers] # Видаляємо непотрібні символи з кожного номера в списку, та додаємо +38
-except Exception:
-    print(f"Програма не може розпізнати ваш номер телефону!")
-    
-print(formated_phone_num)
+def normalize_phone(phone_number):
+    try:
+        regex = "[^0-9]" # регулярний вираз для символів, які ми хочемо видалити
+        phone_number = ["+38" + re.sub(regex, "", number.replace("38", "")) for number in raw_numbers] # Видаляємо непотрібні символи з кожного номера в списку, та додаємо +38
+    except Exception:
+        print(f"Програма не може розпізнати ваш номер телефону!")
+    return phone_number
+
+print(normalize_phone(raw_numbers))
 
