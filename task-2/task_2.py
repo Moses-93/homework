ba84c9b6bc4c18388cf7cc325b64d2f21c7d493d
Moses-93 <moieseenko2001@gmail.com>
2024-03-11 12:23:29 +0200

    task_2_v2

--- task-2/task_2.py
+++ task-2/task_2.py
@@ -4,10 +4,14 @@ try:
     def get_numbers_ticket(min, max, quantity): # створення функції 
         lottery_num = random.sample(range(min, max), k=quantity) # sample - вказує роозмір списку. range - Задає потрібний діапазон
         lottery_num.sort() # сортування списку з числами
+        if min < 1:
+            lottery_num = []
+        elif max > 1000:
+            lottery_num = []
         return lottery_num # повернення відсортованого списку с функції
-    
+
     # введіть числа в такому форматі: '1, 10, 5'. Останнє число повинно бути більше попереднього
-    result = get_numbers_ticket() # викликаємо функцію і вказуємо аргументи
+    result = get_numbers_ticket(0, 1000, 5) # викликаємо функцію і вказуємо аргументи
     print(result)    
 
 except Exception: # перехоплення всіх помилок 
