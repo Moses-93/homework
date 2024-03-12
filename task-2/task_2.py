928f5c9a1cbe94964741cd2619ec9ee099c111fa
Moses-93 <moieseenko2001@gmail.com>
2024-03-12 12:55:59 +0200

    task_2_fics

--- task-2/task_2.py
+++ task-2/task_2.py
@@ -1,7 +1,7 @@
 import random 
 
-try:
-    def get_numbers_ticket(min, max, quantity): # створення функції 
+def get_numbers_ticket(min, max, quantity): # створення функції 
+    try:
         lottery_num = random.sample(range(min, max), k=quantity) # sample - вказує роозмір списку. range - Задає потрібний діапазон
         lottery_num.sort() # сортування списку з числами
         if min < 1:
@@ -9,13 +9,13 @@ try:
         elif max > 1000:
             lottery_num = []
         return lottery_num # повернення відсортованого списку с функції
-
+    except Exception: # перехоплення всіх помилок 
+        print("Ви вказали невірний формат данних. Потрібно ввести цілі числа, як вказано в прикладі!")
     # введіть числа в такому форматі: '1, 10, 5'. Останнє число повинно бути більше попереднього
-    result = get_numbers_ticket(0, 1000, 5) # викликаємо функцію і вказуємо аргументи
-    print(result)    
+result = get_numbers_ticket(1, 1000, 5) # викликаємо функцію і вказуємо аргументи
+print(result)    
 
-except Exception: # перехоплення всіх помилок 
-    print("Ви вказали невірний формат данних. Потрібно ввести цілі числа, як вказано в прикладі!")
+
     
 
 
